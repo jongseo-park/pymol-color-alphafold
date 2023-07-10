0e5775d3eed7384ef11c843dcb78aae230f80985
jongseo <53002411+jongseo-park@users.noreply.github.com>
2023-07-10 11:16:38 +0900

    Update coloraf.py

--- coloraf.py
+++ coloraf.py
@@ -19,11 +19,22 @@ def coloraf(selection="all"):
     The name of the selection/object to color by pLDDT. Default: all
     """
 
-    cmd.color("blue", f"({selection}) and b > 90")
-    cmd.color("cyan", f"({selection}) and b < 90 and b > 70")
-    cmd.color("yellow", f"({selection}) and b < 70 and b > 50")
-    cmd.color("orange", f"({selection}) and b < 50")
 
 
-cmd.extend("plddt", coloraf)
-cmd.auto_arg[0]["coloraf"] = [cmd.object_sc, "object", ""]
+
+    # modified
+    # defind colorset
+    cmd.set_color('n0', [0.051, 0.341, 0.827])
+    cmd.set_color('n1', [0.416, 0.796, 0.945])
+    cmd.set_color('n2', [0.996, 0.851, 0.212])
+    cmd.set_color('n3', [0.992, 0.490, 0.302])
+
+    # apply colorset
+    cmd.color("n0", f"({selection}) and b > 90")
+    cmd.color("n1", f"({selection}) and b < 90 and b > 70")
+    cmd.color("n2", f"({selection}) and b < 70 and b > 50")
+    cmd.color("n3", f"({selection}) and b < 50")
+
+
+cmd.extend('plddt', coloraf)
+cmd.auto_arg[0]['plddt'] = [cmd.object_sc, 'object', '']
